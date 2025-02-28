1fe44f30eef5cac0ed6e6c6a43d6af759723d87a
Yevhenii Edelshteyn <yevheniiedelshteyn17@gmail.com>
2025-02-28 09:09:37 +0100

    Y3298596L Creados y añadidos el resto de archivos base

---
+++ hada-p2/Barco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    internal class Barco
+    {
+        public Dictionary<Coordenada, String> CoordenadasBarco;
+        public string Nombre;
+        public int NumDanyos;
+    }
+}

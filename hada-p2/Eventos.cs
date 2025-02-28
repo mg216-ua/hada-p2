2e130b704e76863e5e7e9e41d93bcba37d5d26b8
Yevhenii Edelshteyn <yevheniiedelshteyn17@gmail.com>
2025-02-28 11:27:02 +0100

    Y3298596L Crude Eventos.cs implementation

--- hada-p2/Eventos.cs
+++ hada-p2/Eventos.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Hada
 {
-    internal class Eventos
+    public class TocadoArgs
     {
+        string nombre;
+        Coordenada cord;
+
+        public TocadoArgs(string _nombre, Coordenada _cord)
+        {
+            nombre = _nombre;
+            cord = _cord;
+        }
+    }
+
+    public class HundidoArgs
+    {
+        string nombre;
+
+        public HundidoArgs(string _nombre)
+        {
+            nombre= _nombre;
+        }
     }
 }

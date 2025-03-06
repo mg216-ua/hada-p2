3f7f290b8044c85718de94b2f251fb7205f56951
Marat Galiullin <mg216@alu.ua.es>
2025-03-06 21:29:42 +0100

    Y8142163Q ficheros Tablero y Game definidos

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -10,14 +10,59 @@ namespace Hada
     {
         private bool finPartida;
 
-        private void gameLoop()
+        public Game()
         {
-
+            finPartida = false;
+            gameLoop();
         }
 
-        private void cuandoEventoFinPartida(...)
+        private void gameLoop()
         {
+            List<Barco> listaBarcos = new List<Barco>();
+            listaBarcos.Add(new Barco("THOR", 3, 'h', new Coordenada(0, 0)));
+            listaBarcos.Add(new Barco("LOKI", 2, 'v', new Coordenada(2, 4)));
+            listaBarcos.Add(new Barco("MAYA", 4, 'h', new Coordenada(5, 1)));
 
+            Tablero tablero = new Tablero(6, listaBarcos);
+
+            //Añadirle el evento fin de partida
+            //tablero.eventoFinPartida += cuandoEventoFinPartida;
+
+            int fila, col;
+
+            while (!finPartida)
+            {
+                Console.WriteLine(tablero.ToString());
+
+                Console.WriteLine("Introduce la coordenada a la que disparar FILA,COLUMNA ('S' para Salir)");
+                string coordStr = Console.ReadLine();
+
+                if (coordStr.Equals("S"))
+                {
+                    finPartida = true;
+                }
+                else
+                {
+                    string[] fila_col = coordStr.Split(',');
+
+                    if (fila_col.Length == 2 && int.TryParse(fila_col[0], out fila) && int.TryParse(fila_col[1], out col))
+                    {
+
+                        Coordenada coordDisparo = new Coordenada(fila, col);
+                        tablero.Disparar(coordDisparo);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Formato incorrecto. El formato debe ser NUMERO,NUMERO");
+                    }
+                }
+            }
+        }
+
+        private void cuandoEventoFinPartida(EventArgs e)
+        {
+            Console.WriteLine("PARTIDA FINALIZADA!!");
+            finPartida = true;
         }
     }
 }
--- hada-p2/Tablero.cs
+++ hada-p2/Tablero.cs
@@ -8,28 +8,166 @@ namespace Hada
 {
     internal class Tablero
     {
+        private int _tamTablero;
         public int TamTablero
         {
-            get { return TamTablero; }
-
-            private set 
+            get { return _tamTablero; }
+            set
             {
-                if (value <= 4 && value >= 9)
+                if (value >= 4 && value <= 9)
                 {
-                    
+                    _tamTablero = value;
                 }
             }
         }
 
-        private List<Coordenada> coordenadasDisparadas;
-        private List<Coordenada> coordenadasTocadas;
-        private List<Barco> barcos;
-        private List<Barco> barcosEliminados;
-        private Dictionary<Coordenada, string> casillasTablero;
+        private List<Coordenada> CoordenadasDisparadas;
+        private List<Coordenada> CoordenadasTocadas;
+        private List<Barco> Barcos;
+        private List<Barco> BarcosEliminados;
+        private Dictionary<Coordenada, string> CasillasTablero;
+        public event EventHandler<EventArgs> eventoFinPartida;
 
         public Tablero(int tamTablero, List<Barco> barcos)
         {
+            this.TamTablero = tamTablero;
+            this.Barcos = barcos;
 
+            CoordenadasDisparadas = new List<Coordenada>();
+            CoordenadasTocadas = new List<Coordenada>();
+            BarcosEliminados = new List<Barco>();
+
+            CasillasTablero = new Dictionary<Coordenada, string>();
+
+            inicializarCasillasTablero();
+
+            foreach (var barco in Barcos)
+            {
+                barco.eventoTocado += cuandoEventoTocado;
+                barco.eventoHundido += cuandoEventoHundido;
+            }
+
+        }
+
+        private void inicializarCasillasTablero()
+        {
+            CasillasTablero.Clear();
+
+            for (int i = 0; i < TamTablero; i++)
+            {
+                for (int j = 0; j < TamTablero; j++)
+                {
+                    CasillasTablero[new Coordenada(i, j)] = "AGUA";
+                }
+            }
+
+            foreach (var barco in this.Barcos)
+            {
+                foreach (var coordenada in barco.CoordenadasBarco)
+                {
+                    if (coordenada.Key.Fila >= 0 && coordenada.Key.Columna < TamTablero
+                        && coordenada.Key.Columna >= 0 && coordenada.Key.Columna < TamTablero)
+                    {
+                        CasillasTablero[coordenada.Key] = barco.Nombre;
+                    }
+                }
+            }
+        }
+
+        public void Disparar(Coordenada c)
+        {
+            if (c.Fila >= 0 && c.Columna < TamTablero
+                        && c.Columna >= 0 && c.Columna < TamTablero)
+            {
+                foreach (var barco in Barcos)
+                {
+                    if (barco.CoordenadasBarco.ContainsKey(c))
+                    {
+                        barco.Disparo(c);
+                        CoordenadasDisparadas.Add(c);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("La coordenada " + c.ToString() + " está fuera de las dimensiones del tablero.");
+
+            }
+        }
+
+        public string DibujarTablero()
+        {
+            string texto = "";
+            int nColumna = 0;
+
+            foreach (var casilla in CasillasTablero)
+            {
+                texto += "[" + casilla.Value + "]";
+                nColumna++;
+
+                if (nColumna == TamTablero)
+                {
+                    nColumna = 0;
+                    texto += "\n";
+                }
+            }
+
+            return texto;
+        }
+
+        public override string ToString()
+        {
+            string texto = "\n\n";
+
+            foreach (var barco in Barcos)
+            {
+                texto += barco.ToString() + "\n";
+            }
+
+            texto += "\nCoordenadas disparadas: ";
+            foreach (var coordenada in CoordenadasDisparadas)
+            {
+                texto += coordenada.ToString() + " ";
+            }
+
+            texto += "\nCoordenadas tocadas: ";
+            foreach (var coordenada in CoordenadasTocadas)
+            {
+                texto += coordenada.ToString() + " ";
+            }
+
+
+            texto += "\n\nCASIILAS TABLERO\n";
+            texto += "------\n";
+            texto += DibujarTablero();
+
+            return texto;
+        }
+
+        private void cuandoEventoTocado(object sender, TocadoArgs e)
+        {
+            CasillasTablero[e.CoordenadaImpacto] = e.Nombre + "_T";
+
+            CoordenadasTocadas.Add(e.CoordenadaImpacto);
+
+            Console.WriteLine("TABLERO: Barco [" + e.Nombre + "] tocado en Coordenada: [" + e.CoordenadaImpacto.ToString() + "]");
+        }
+
+        private void cuandoEventoHundido(object sender, HundidoArgs e)
+        {
+            Console.WriteLine("TABLERO: Barco [" + e.Nombre + "] hundido!!");
+
+            Barco hundido = Barcos.First(b => b.Nombre == e.Nombre);
+
+            if (hundido != null && BarcosEliminados.Contains(hundido))
+            {
+                BarcosEliminados.Add(hundido);
+            }
+
+            if (BarcosEliminados.Count == Barcos.Count)
+            {
+                eventoFinPartida?.Invoke(this, EventArgs.Empty);
+            }
         }
     }
 }
